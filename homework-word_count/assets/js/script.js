a9041d885538b8215a97b2d53cc4a3d138612e90
95rave <ravanaap@code.edu.az>
2023-06-06 01:08:31 +0400

    Js function change

--- homework-word_count/assets/js/script.js
+++ homework-word_count/assets/js/script.js
@@ -5,9 +5,12 @@ function wordCount() {
   let words = txt.trim().split(/\s+/);
   let count = words.length;
 
-  if (count > 3) {
-    document.getElementById("wcount").innerHTML = "Limit exceeded";
+  if (count >=3) {
+    let truncatedText = words.slice(0, 30).join(" ");
+    document.getElementById("txt").value = truncatedText;
+    document.getElementById("wcount").innerHTML = "Limit is 3 words";
   } else {
     document.getElementById("wcount").innerHTML = count;
   }
 }
+
